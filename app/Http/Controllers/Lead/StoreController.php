ebc849233b2a914aec000802657b05429e0e99ec
Andrew Valentovich <andrewvalentovich14@gmail.com>
2023-07-02 22:46:25 +0300

    feat: Complete integration with Bitrix24

--- app/Http/Controllers/Lead/StoreController.php
+++ app/Http/Controllers/Lead/StoreController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Lead;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Lead\StoreRequest;
+use App\Http\Services\Bitrix\CreateLead;
 use App\Models\Lead;
 use Carbon\Carbon;
 
@@ -17,11 +18,16 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $data['birth_day'] = Carbon::parse($data['birth_day']);
 
-        // При подстановке такого же номера телефона для нового лида но с другим индексом (+7 вместо 8 и наоборот) создастся лид.
-        $lead = Lead::firstOrCreate(['phone' => $data['phone'], 'email' => $data['email']], $data);
-        dd($lead);
+        // Отправляем запрос на сервер Bitrix24 и получаем ответ в $response
+        $response = CreateLead::sendRequest("GET", "crm.lead.add", $data);
+
+        // Если нет ошибок - в $responce получаем false и выполняем сценарий ниже
+        if(!$response) {
+            $data['birth_day'] = Carbon::parse($data['birth_day']);
+            // При подстановке такого же номера телефона для нового лида но с другим индексом (+7 вместо 8 и наоборот) создастся лид.
+            Lead::firstOrCreate(['phone' => $data['phone'], 'email' => $data['email']], $data);
+        }
 
     }
 }
---
+++ app/Http/Services/Bitrix/CreateLead.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Http\Services\Bitrix;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Facades\Log;
+
+class CreateLead
+{
+    /**
+     * Функция generateQuery возвращает параметры запроса
+     * @param array $data
+     * @return array
+     */
+    public static function generateQuery(array $data)
+    {
+        return [
+            "fields" => [
+                "NAME" => $data['name'],
+                "SECOND_NAME" => $data['surname'],
+                "LAST_NAME" => $data['patronymic'],
+                "BIRTHDATE" => $data['birth_day'],
+                "ASSIGNED_BY_ID" => 1,
+                "PHONE" => [ [ "VALUE" => $data['phone'], "VALUE_TYPE" => "WORK" ] ],
+                "EMAIL" => [[ "VALUE" => $data['email'], "VALUE_TYPE" => "WORK" ] ],
+                "COMMENTS" => $data['comment']
+            ],
+            "params" => ["REGISTER_SONET_EVENT" => "Y"]
+        ];
+    }
+
+    /**
+     * @param string $method
+     * @param string $crm_func
+     * @param array $data
+     * @return false|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendRequest($method, $crm_func, array $data)
+    {
+        $client = new Client();
+        $url = env('CLIENT_REST_WEB_HOOK_URL') . $crm_func . '.json'; // Формируем url для отправки запроса
+
+        try {
+            // Отправляем запрос
+            $client->request($method, $url, [
+                "query" => static::generateQuery($data)
+            ]);
+        } catch (ClientException $e) { // Ловим ошибки 4хх
+            $error = ($e->getResponse()->getReasonPhrase());
+            Log::error("Ошибка при создании лида, сработало исключение в Services/Bitrix/CreateLead callRequest()", [
+                'error' => true,
+                'line' => 60
+            ]);
+        }
+
+        // Возвращаем false, если всё хорошо, или возвращаем $error, если словили искоючение
+        return isset($error) ? false : $error;
+    }
+
+
+}
